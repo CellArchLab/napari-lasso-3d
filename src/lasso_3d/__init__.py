13cbbb576d5ca027e1a24080a4127547acbcf50d
LorenzLamm <lorenz.lamm@googlemail.com>
2024-06-27 09:54:14 +0200

    remove redundant widgets

--- src/lasso_3d/__init__.py
+++ src/lasso_3d/__init__.py
@@ -1,17 +1,13 @@
 __version__ = "0.0.1"
 
-from ._reader import napari_get_reader
 from ._widget import (
-    ExampleQWidget,
-    ImageThreshold,
-    threshold_autogenerate_widget,
-    threshold_magic_widget,
+    Lasso3D,
 )
 
 __all__ = (
-    "napari_get_reader",
-    "ExampleQWidget",
-    "ImageThreshold",
-    "threshold_autogenerate_widget",
-    "threshold_magic_widget",
+    # "napari_get_reader",
+    "Lasso3D",
+    # "ImageThreshold",
+    # "threshold_autogenerate_widget",
+    # "threshold_magic_widget",
 )
